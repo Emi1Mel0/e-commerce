d7ff81ae8f09e3292f9e82ce60071dc349684d21
Emi1Mel0 <emilwagdy11@gmail.com>
2023-10-14 15:41:31 +0300

    all nessecart files without doubt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from "./auth.guard";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { ProductsComponent } from "./products/products.component";
+import { CartComponent } from "./cart/cart.component";
+import { CategoriesComponent } from "./categories/categories.component";
+import { NotfoundComponent } from "./notfound/notfound.component";
+import { BrandsComponent } from "./brands/brands.component";
+import { SignupComponent } from "./signup/signup.component";
+import { LoginComponent } from "./login/login.component";
+import { ProductdetailsComponent } from "./productdetails/productdetails.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", canActivate: [AuthGuard], component: HomeComponent },
+  {
+    path: "categories",
+    canActivate: [AuthGuard],
+    component: CategoriesComponent,
+  },
+  { path: "brands", canActivate: [AuthGuard], component: BrandsComponent },
+  { path: "cart", canActivate: [AuthGuard], component: CartComponent },
+  { path: "products", canActivate: [AuthGuard], component: ProductsComponent },
+  {
+    path: "productdetails/:id",
+    canActivate: [AuthGuard],
+    component: ProductdetailsComponent,
+  },
+  { path: "signup", component: SignupComponent },
+  { path: "login", component: LoginComponent },
+  { path: "**", component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { BrandsComponent } from './brands/brands.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ProductdetailsComponent } from './productdetails/productdetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    LoginComponent,
+    SignupComponent,
+    FooterComponent,
+    CartComponent,
+    BrandsComponent,
+    CategoriesComponent,
+    NotfoundComponent,
+    ProductdetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
